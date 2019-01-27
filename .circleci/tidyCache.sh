c169dfc0e754ac3f8a3742525aa55c0b548e8cd0
Luke Lau <luke_lau@icloud.com>
2019-01-27 16:18:11 +0000

    Don't error if cache doesn't exist

--- .circleci/tidyCache.sh
+++ .circleci/tidyCache.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 if [ "$STACK_FILE" != stack-8.6.3.yaml ] && [ "$STACK_FILE" != stack.yaml ]; then
-    rm -r ~/.stack/precompiled/*/ghc-8.6.3
-    rm -r ~/.stack/programs/*/ghc-8.6.3*
-    rm -r ~/.stack/loaded-snapshot-cache/*/ghc-8.6.3
-    rm -r .stack-work/install/*/*/8.6.3
+    # use -f so it doesn't error if it doesn't exist
+    rm -rf ~/.stack/precompiled/*/ghc-8.6.3
+    rm -rf ~/.stack/programs/*/ghc-8.6.3*
+    rm -rf ~/.stack/loaded-snapshot-cache/*/ghc-8.6.3
+    rm -rf .stack-work/install/*/*/8.6.3
 fi
